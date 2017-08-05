c98cc01ee7b0d30ce933d94863af4ae77f352996
hongyi zhang <hzhan016@gmail.com>
2017-08-05 15:52:06 -0700

    adding more stuff

--- src/containers/Game/index.js
+++ src/containers/Game/index.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Game from '../../lib'
-
+import { Jumbotron } from 'react-bootstrap'
+import './Game.css'
 class GameView extends Component {
+    constructor (props) {
+        super(props)
+        this.game = null
+    }
 
     componentDidMount () {
-        const test = new Game()
+        // this.game = new Game({ width: window.innerWidth, height: window.innerHeight }, '#game-start')
+        this.game = new Game({ width: 1024, height: 768 }, '#game-start')
+        
+    }
+    componentWillReceiveProps (nextProps) {
+        console.log(this.game)
+        
+
     }
     render () {
-        return <div>
-            lkasjdf;lkasdf
+        return <div className='container-fluid'>
+            <Jumbotron>
+                <div className='game-container' id='game-start' />
+            </Jumbotron>
+            <hr />
+            <Jumbotron>
+                Game page
+                inside
+
+            </Jumbotron>
         </div>
     }
 }
--- src/lib/Game.js
+++ src/lib/Game.js
@@ -1,15 +1,43 @@
+var THREE = null
 const sanityCheck = () => new Promise ((resolve, reject) => {
-    console.log(window.THREE)
-    resolve()
+    THREE = window.THREE
+    window.THREE !== undefined ? resolve() : reject(new Error('Cannot find threejs'))
 })
 
+const ACTIONS = {
+    STOP: 'STOP',
+    START: 'START'
+}
+
 export class Game {
-    constructor () {
+    constructor ( {width, height }, idName) {
         sanityCheck()
-        console.log('here')
+        .then(() => {
+                console.log('Check[1] complete')
+                this.width = width
+                this.height = height
+                this.id = idName
+                this.status = ACTIONS.START
+
+                this.scene = new THREE.Scene()
+                this.camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 1000)
+                this.renderer = new THREE.WebGLRenderer()
+
+            }
+        )
+        .catch(err => {
+            console.error('failed to load threejs', err)
+        })
+        this.status = ACTIONS.STOP
+        this.width = null
+        this.height = null
+        this.id = null
+
+        this.scene = null
+        
     }
     init () {
-
+        console.log(this.width, this.height, this.idName)
     }
 }
 
