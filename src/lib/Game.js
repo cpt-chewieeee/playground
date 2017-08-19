ffb863324477f90b9557b41750ae7a12f1a327e9
hongyi zhang <hzhan016@gmail.com>
2017-08-18 21:52:34 -0700

    set up import and export

--- src/lib/Game.js
+++ src/lib/Game.js
@@ -1,3 +1,6 @@
+import Demo from './Demo'
+
+console.log(Demo)
 var THREE = null
 const sanityCheck = () => new Promise ((resolve, reject) => {
     THREE = window.THREE
@@ -13,8 +16,7 @@ export class Game {
     constructor ( { width, height }, idName) {
       sanityCheck()
         .then(() => {
-          console.log('Check[1] complete')
-          console.log('initializing...', width, height, idName)
+          console.log('-----sanity-check-----')
           this.width = width
           this.height = height
           this.id = idName
@@ -24,10 +26,10 @@ export class Game {
           this.camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
           
           this.renderer = new THREE.WebGLRenderer()
-          console.log(this.renderer)
-          // this.renderer.setClearColorHex()
+        
           this.renderer.setClearColor(new THREE.Color(0xEEEEEE))
           this.renderer.setSize(width, height)
+          this.renderer.shadowMapEnabled = true 
           
           this.axisHelper = new THREE.AxisHelper(20)
 
@@ -48,58 +50,33 @@ export class Game {
       
       
     }
-    getPlane () {
-      console.log('-----get-a-plane-----')
-      var planeGeometry = new THREE.PlaneGeometry(60, 20)
-      var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc })
-      var plane = new THREE.Mesh(planeGeometry, planeMaterial)
-      plane.rotation.x = -0.5 * Math.PI
-      plane.position.x = 15
-      plane.position.y = 0
-      plane.position.z = 0
-      return plane
-    }
+    
 
-    makeACube () {
-      console.log('-----making-a-cube-----')
-      var cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
-      var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true})
-      var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+    
 
-      // position the cube
-      cube.position.x = -4
-      cube.position.y = 3
-      cube.position.z = 0
+    
 
-      return cube
-    }
-
-    makeASphere () {
-      // create a sphere
-      var sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
-      var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true})
-      var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-
-      // position the sphere
-      sphere.position.x = 20
-      sphere.position.y = 4
-      sphere.position.z = 2
-
-      return sphere
-    }
-
+    
+    
     init () {
       return new Promise ((resolve, reject) => {
         this.scene.add(this.axisHelper)
-        this.scene.add(this.getPlane())
-        this.scene.add(this.makeACube())
-        this.scene.add(this.makeASphere())
+        // this.scene.add(this.getPlane())
+        // this.scene.add(this.makeACube())
+        // this.scene.add(this.makeASphere())
+
+        Object.keys(Demo).map(key => {
+          this.scene.add(Demo[key]())
+        })
 
         this.camera.position.x = -30
         this.camera.position.y = 40
         this.camera.position.z = 30
         this.camera.lookAt(this.scene.position)
       
+        // this.scene.add(this.makeAmbientLight())
+        // this.scene.add(this.makeSpotLight())
+
         document.getElementById(this.id).appendChild(this.renderer.domElement)
 
         this.renderer.render(this.scene, this.camera)
@@ -109,8 +86,6 @@ export class Game {
     }
     start () {
       this.init().then(() => console.log('success'))
-      console.log('---------------starting---------------')
-      console.log(this)
     }
 }
 
